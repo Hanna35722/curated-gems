6396d057d9199cdc8b9eb338ce26f300bdc784d7
Hanna35722 <1547844230@qq.com>
2025-09-27 23:30:53 +0800

    Update app.js

--- v2/app.js
+++ v2/app.js
@@ -174,9 +174,9 @@ function applyAndRender() {
     render(view);
     renderSources(['all', ...new Set(raw.map(x => x.source))]);
     // 彩蛋：输入 magic 试试看
-if (query === 'magic') {
-  alert('✨ 哇！你发现了隐藏功能！');
-}
+    if (query === 'magic') {
+      alert('✨ 哇！你发现了隐藏功能！');
+    }
 
 /**
  * 渲染数据源选择器
